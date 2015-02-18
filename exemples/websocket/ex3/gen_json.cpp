f30bcab6520809892c5da44fd7a5f6678619a494
8gif128 <8gif128@gmail.com>
2015-02-18 14:58:30 -0500

    no message

--- exemples/websocket/ex3/gen_json.cpp
+++ exemples/websocket/ex3/gen_json.cpp
@@ -6,11 +6,7 @@
 //Les variables d'environnement contiennent des infos également
 
 int main() {
-    // Disable output buffering.
-    //setbuf(stdout, NULL);
 
-
-//todo fix le json
 char * json = 
 "{"
 "    \"nodes\": ["
@@ -39,16 +35,6 @@ char * json =
 "    ]"
 "}";
 
-    // Disable output buffering.
-    //setbuf(stdout, NULL);
-
-	int sommets = 0, densite = 0;
-	//Lire les specs du graphe
-	scanf("%d %d", &sommets, &densite);
-
-	//Generer graphe
-	//printf("%d %d\n", sommets, densite);
 	puts(json);
-
     return 0;
 	}
---
+++ exemples/websocket/ex4/README.md
@@ -0,0 +1,8 @@
+A propos de cet exemple
+=========================
+Cet exemple utilise un executable produit avec le langage C++ pour renvoyer un JSON par websockets a Javascript.
+Ce JSON est ensuite manipulé pour devenir un graphe avec la librairie Springy
+http://getspringy.com/
+
+Une autre librairie possible d'utilisation est :
+http://graphplayground.herokuapp.com/ (Style plus académique, mais aucune documentation)
